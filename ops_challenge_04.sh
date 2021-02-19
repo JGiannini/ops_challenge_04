4cb975164080d4a6ef07aa480bba95875368153a
Jamie <jgianninidesign@gmail.com>
2021-02-18 16:55:15 -0800

    Initial commit

---
+++ ops_challenge_04.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Objectives:
+# [] Create four directories: dir1, dir2, dir3, dir4
+# [] Put the four directories in an array
+# [] References the array variable to create a new .txt file in each directory
+
